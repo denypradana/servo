ce9c6faf928de61b0d63499dbc9edbcd705ac9bb
denypradana <pradana.deny@gmail.com>
2018-08-08 08:57:32 +0700

    First Commit

---
+++ servo.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo myservo;
+
+void setup() {
+  // put your setup code here, to run once:
+myservo.attach(2);
+myservo.write(00);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*
+  myservo.write(00);
+  delay(100);
+  myservo.write(10);
+  delay(100);
+  myservo.write(20);
+  delay(100);
+  myservo.write(30);
+  delay(100);
+  myservo.write(40);
+  delay(100);
+  myservo.write(50);
+  delay(100);
+  myservo.write(60);
+  delay(100);
+  myservo.write(70);
+  delay(100);
+  myservo.write(80);
+  delay(100);
+  myservo.write(90);
+  delay(100);
+  myservo.write(100);
+  delay(100);
+  myservo.write(110);
+  delay(100);
+  myservo.write(120);
+  delay(100);
+  myservo.write(130);
+  delay(100);
+  myservo.write(140);
+  delay(100);
+  myservo.write(150);
+  delay(100);
+  myservo.write(160);
+  delay(100);
+ */
+  
+}
